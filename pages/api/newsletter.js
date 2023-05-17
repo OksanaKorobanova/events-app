983215771386e9f6ae9d57f5823ec1c08c7af0b6
Oksana Korobanova <58445023+OksanaKorobanova@users.noreply.github.com>
2023-05-17 22:34:52 +0200

    added mongo, store newsletter

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,11 +1,24 @@
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const email = req.body.email;
     if (!email || !email.includes('@')) {
       res.status(422).json({ message: 'Invalid email' });
       return;
     }
-    console.log(email);
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://oksanavitol:LiS8yU0CQWWTlGZr@cluster0.ydeabvx.mongodb.net/?retryWrites=true&w=majority'
+    );
+    const db = client.db('newsletter');
+
+    await db.collection('emails').insertOne({ email: email });
+
+    client.close();
+
     res.status(201).json({ message: 'Signed up' });
   }
 }
+
+export default handler;
